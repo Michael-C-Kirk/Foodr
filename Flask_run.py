5bfb3d39d95968b49f2b437769ab004311a2f3fb
aanimesh23 <animesha@uci.edu>
2019-02-16 15:09:22 -0800

    Implementing quick

--- Flask_run.py
+++ Flask_run.py
@@ -1,10 +1,36 @@
 from flask import Flask, request, redirect, render_template
+import APIcalls as api
+import parser as parse
+
 
 app = Flask(__name__)
-
+details = dict()
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/quick')
+def quick():
+    print("IN QUICK!!!")
+    foodDict = api.call("5000", "fast food")
+    binfo = parse.ratingParser(foodDict, 3)[1]
+    print(binfo)
+    return render_template('quick_response.html', name = binfo[0][0], image = binfo[0][1])
+    #return render_template('quick_response.html')
+
+    
+@app.route('/choose')
+def choose():
+    return 'results go here'
+
+@app.route('/details_given', methods = ['GET'])
+def detailed():
+    details['price'] = request.form['price']
+    details['foodtype'] = request.form['cuisine']
+
+    print(details)
+    #return details
+    return redirect('/choose')
 
 if __name__ == '__main__':
     app.run()
