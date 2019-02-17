6843036008c12e706beccc2d25f9cd5bc5a70e8b
aanimesh23 <animesha@uci.edu>
2019-02-17 02:03:52 -0800

    MY BACK HURTS
    added thumb up functionality and fixed radio buttoms

--- Flask_run.py
+++ Flask_run.py
@@ -4,11 +4,22 @@ import decode as dcd
 import decision_tree as dt
 import categories as c
 from random import randrange
+from helper import time_parse, age_paser
+import restarauntURL as ru
 
 KEY_WORD_LIST = ["sushi", "spaghetti", "taco", "ramen", "sandwich", "icecream", "kbbq", "fish", "soup", "pancake"]
 clf = None
 app = Flask(__name__)
 details = dict()
+
+
+@app.route('/thumpup')
+def upbutton():
+    print("AFTER UP")
+    print(REST_ID)
+    url = ru.getRestarauntURL(REST_ID)
+    return redirect(url)
+        
 @app.route('/')
 def index():
     clt = dt.train_initial_data()
@@ -17,24 +28,36 @@ def index():
 def surprise():
     foodDict = api.call("7000", KEY_WORD_LIST[randrange(0, len(KEY_WORD_LIST))])
     binfo = dcd.ratingParser(foodDict, 4)[1]
+    if len(binfo) == 0:
+        return render_template('surprise.html', name = "All locations are closed at the moment", image = "We appologize for the inconvenience", rating = "")
     placeNum = randrange(0, len(binfo))
-    return render_template('surprise.html', name = binfo[placeNum][0], image = binfo[placeNum][2], rating = binfo[placeNum][1])
+    REST_ID = binfo[placeNum][3]
+    url = ru.getRestarauntURL(REST_ID)
+    return render_template('surprise.html', name = binfo[placeNum][0], image = binfo[placeNum][2], rating = binfo[placeNum][1], URL = url)
     
 @app.route('/caffeine')
 def coffee():
     print("IN COFFEE!!!")
     foodDict = api.call("5000", "cafe and coffee")
     binfo = dcd.ratingParser(foodDict, 3.5)[1]
+    if len(binfo) == 0:
+        return render_template('caffeine.html', name = "All locations are closed at the moment", image = "We appologize for the inconvenience", rating = "")
     placeNum = randrange(0, len(binfo))
-    return render_template('caffeine.html', name = binfo[placeNum][0], image = binfo[placeNum][2], rating = binfo[placeNum][1])
-
+    REST_ID = binfo[placeNum][3]
+    url = ru.getRestarauntURL(REST_ID)
+    return render_template('caffeine.html', name = binfo[placeNum][0], image = binfo[placeNum][2], rating = binfo[placeNum][1], URL = url)
+    
 @app.route('/quick')
 def quick():
     print("IN QUICK!!!")
     foodDict = api.call("5000", "fast food")
     binfo = dcd.ratingParser(foodDict, 3.5)[1]
+    if len(binfo) == 0:
+        return render_template('quick_response.html', name = "All locations are closed at the moment", image = "We appologize for the inconvenience", rating = "")
     placeNum = randrange(0, len(binfo))
-    return render_template('quick_response.html', name = binfo[placeNum][0], image = binfo[placeNum][2], rating = binfo[placeNum][1])
+    REST_ID = binfo[placeNum][3]
+    url = ru.getRestarauntURL(REST_ID)
+    return render_template('quick_response.html', name = binfo[placeNum][0], image = binfo[placeNum][2], rating = binfo[placeNum][1], URL = url)
     #return render_template('quick_response.html')
 
     
@@ -52,10 +75,11 @@ def detailed():
         details['mood'] = 0
     else:
         print("ERROR")
-    
-    details['price'] = request.form['price']
-    #details['foodtype'] = request.form['cuisine']
 
+    if request.form['price'] == None:
+        print("WHAT?")
+    details['price'] = request.form['price']
+    
     print(details)
     #return details
     return redirect('/choose')
--- restarauntURL.py
+++ restarauntURL.py
@@ -12,6 +12,7 @@ def getRestarauntURL(restarauntID: str):
 		restarauntDict: string representing a single restaraunt place_id
 		return: string representing restaraunt url in google
 	'''
+	print(restarauntID)
 	BASE_URL_REVIEW = BASE_URL_REVIEWS
 	query_parameters = [
 		('placeid', restarauntID), ('fields', 'name,url'), ('key', GOOGLE_API_KEY)]
@@ -19,6 +20,7 @@ def getRestarauntURL(restarauntID: str):
 	placeDetailsURL = BASE_URL_REVIEW + urllib.parse.urlencode(query_parameters)
 
 	placeDetailsDict = api.get_result(placeDetailsURL)
+	print(placeDetailsDict)
 	return placeDetailsDict['result']['url'] 
 
 if __name__ == '__main__':
