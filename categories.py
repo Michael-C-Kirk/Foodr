78514c99714959115522e0f69acb9f7c17ab894d
aanimesh23 <animesha@uci.edu>
2019-02-16 19:50:34 -0800

    categories for food added
    add categories and sub categories as dictionary

---
+++ categories.py
@@ -0,0 +1,7 @@
+categories = {'american': ['bbq', 'breakfast brunch', 'pancakes', 'burger', 'chicken_wings', 'waffles', 'fish n chips', 'pizza', 'salad', 'sandwiches'],
+              'asian': ['chinese', 'dimsum', 'filipino','hotpot','japanese', 'ramen', 'korean', 'sushi', 'thai'],
+              'indian': ['indian', 'kebab'],
+              'italian': ['italian','pizza', 'pasta'],
+              'mexican': ['mexican', 'taco','burrito', 'quesadilla'],
+              'dessert': ['icecream', 'cheesecake', 'cake', 'creme brulee'],
+              'french': ['french', 'fondue']}
