0d76af3fb3da994396ae3d5e0e191138d410a436
LoyaFrancisco <Loyaa11@gmail.com>
2019-02-16 22:01:59 -0800

    category updates
    -Created conversions between symbols and categories
    -Updated categories dictionary

--- categories.py
+++ categories.py
@@ -1,7 +1,15 @@
 categories = {'american': ['bbq', 'breakfast brunch', 'pancakes', 'burger', 'chicken_wings', 'waffles', 'fish n chips', 'pizza', 'salad', 'sandwiches'],
               'asian': ['chinese', 'dimsum', 'filipino','hotpot','japanese', 'ramen', 'korean', 'sushi', 'thai'],
-              'indian': ['indian', 'kebab'],
+              'bar':['bar'],
               'italian': ['italian','pizza', 'pasta'],
-              'mexican': ['mexican', 'taco','burrito', 'quesadilla'],
-              'dessert': ['icecream', 'cheesecake', 'cake', 'creme brulee'],
-              'french': ['french', 'fondue']}
+              'dessert': ['icecream', 'cheesecake', 'cake', 'creme brulee']}
+symbol_to_categories: {1:'american',
+                       2:'asian',
+                       3:'bar',
+                       4:'italian',
+                       5:'dessert'}
+categories_to_symbols: {'american':1,
+                        'asian':2,
+                        'bar':3,
+                        'italian':4,
+                        'dessert':5}
